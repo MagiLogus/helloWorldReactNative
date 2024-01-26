22a60a57534dffcb13353efadc63b5ea2341e172
Paulo Gonçalves <paulo.oliveira.phgo@gmail.com>
2024-01-25 21:11:06 -0300

    projeto finalizado

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [rotation, setRotation] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRotation(prevRotation => (prevRotation + 2) % 360);
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Image
+          style={[styles.logo, { transform: [{ rotate: `${rotation}deg` }] }]}
+          source={require('./src/assets/logo.png')}
+        />
+        <Text style={styles.text}>Hello, World!</Text>
+        <TextInput style={styles.input}
+          color='black'
+          placeholder='Digite aqui o seu texto'
+        />
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnText}>Pressione</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  content: {
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100,
+  },
+  logo: {
+    width: 120,
+    height: 120
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: '500',
+    color: 'black',
+    marginTop: 150,
+    marginBottom: 100,
+  },
+  input: {
+    width: '90%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#5C6672',
+    borderRadius: 8,
+    paddingLeft: 20,
+  },
+  btn: {
+    marginTop: 40,
+    backgroundColor: 'black',
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    borderRadius: 8,
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: '700'
   },
 });
